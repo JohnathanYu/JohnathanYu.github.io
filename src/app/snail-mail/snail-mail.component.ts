6af7c95253e460fef5ac9b3653aeb2c808a54157
Johnathan Yu <johnathanzyu@gmail.com>
2024-01-05 15:57:32 -0500

    added game stability

--- src/app/snail-mail/snail-mail.component.ts
+++ src/app/snail-mail/snail-mail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'snail-mail',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./snail-mail.component.css']
 })
 export class SnailMailComponent implements OnInit {
-  DEBUGMODE: boolean = true;
+  DEBUGMODE: boolean = false;
 
   snails: number = 0;
   totalAquiredSnails: number = 0;
@@ -35,15 +36,18 @@ export class SnailMailComponent implements OnInit {
   hundredthShellFlag: boolean = false;
 
   ngOnInit(): void {
-  setInterval(this.secondTick, 1000);
-  // setInterval(fiveSecondTick, 5000);
+    interval(1000).subscribe(()=> {
+      this.secondTick();
+    })
+    interval(5000).subscribe(()=> {
+      this.fiveSecondTick();
+    })
   }
   
   //Wild snail generation
   secondTick(): void {
     if(this.wildSnails < this.wildSnailsMax) {
       this.wildSnailsDelta = this.wildSnailsBaseGrowth * this.wildSnails * ((this.wildSnailsMax - this.wildSnails) / this.wildSnailsMax)
-      // document.getElementById("snailsDeltaDisplay").innerHTML = "Delta: " + wildSnailsDelta;
       if (this.wildSnailsDelta > 1) {
         this.wildSnails += this.wildSnailsDelta;
       } else {
@@ -52,109 +56,111 @@ export class SnailMailComponent implements OnInit {
       if (this.wildSnails > this.wildSnailsMax) {
         this.wildSnails = this.wildSnailsMax;
       }
-      this.wildSnailsDisplay = Math.floor(this.wildSnails);
+      this.updateWildSnailsText();
     }
   }
   
-  // function fiveSecondTick() {
-  //   //Shell production
-  //   shellDelta = Math.floor(Math.sqrt(farmSnails));
-  //   if(isNaN(shellDelta)) {
-  //     shellDelta = 0;
-  //   }
-  //   shells += shellDelta;
-  //   updateShellsText();
-  //   if(!firstShellFlag && shells >= 1) {
-  //     firstShellFlag = true;
-  //     document.getElementById("dialogDisplay").innerHTML = "Hmm, that's strange. The snails in the farm aren't making new snails�all you find are shells.";
-  //     document.getElementById('shellsDisplay').style.display='block';
-  //   } else if(!hundredthShellFlag && shells >= 100) {
-  //     hundredthShellFlag = true;
-  //     document.getElementById("dialogDisplay").innerHTML = "Maybe you can study the shells to understand what's going on. And maybe learn more about snails in general.";
-  //   }
-  // }
+  fiveSecondTick(): void {
+    //Shell production
+    let shellDelta = Math.floor(Math.sqrt(this.farmSnails));
+    if(isNaN(shellDelta)) {
+      shellDelta = 0;
+    }
+    this.shells += shellDelta;
+    // updateShellsText();
+    if(!this.firstShellFlag && this.shells >= 1) {
+      this.firstShellFlag = true;
+      // document.getElementById("dialogDisplay").innerHTML = "Hmm, that's strange. The snails in the farm aren't making new snails�all you find are shells.";
+      // document.getElementById('shellsDisplay').style.display='block';
+    } else if(!this.hundredthShellFlag && this.shells >= 100) {
+      this.hundredthShellFlag = true;
+      // document.getElementById("dialogDisplay").innerHTML = "Maybe you can study the shells to understand what's going on. And maybe learn more about snails in general.";
+    }
+  }
   
   
-  // huntSnail(): void {
-  //   if(wildSnails >= 1) {
-  //     wildSnails--;
-  //     updateWildSnailsText();
-  //     gainSnails(1);
-  //   } else if(displayWildSnailsFlag == false){
-  //      document.getElementById("dialogDisplay").innerHTML = "Looks like there aren't any snails today. Maybe the snails need a while to reproduce.";
-  //      document.getElementById('wildSnailsDisplay').style.display='inline'
-  //      displayWildSnailsFlag = true;
-  //   }
-  // }
+  huntSnail(): void {
+    if(this.wildSnails >= 1) {
+      this.wildSnails--;
+      this.updateWildSnailsText();
+      this.gainSnails(1);
+    } else if(this.displayWildSnailsFlag == false){
+      //  document.getElementById("dialogDisplay").innerHTML = "Looks like there aren't any snails today. Maybe the snails need a while to reproduce.";
+      //  document.getElementById('wildSnailsDisplay').style.display='inline'
+       this.displayWildSnailsFlag = true;
+    }
+  }
   
-  // function buildFarm() {
-  //   if(snails >= 1) {
-  //     loseSnails(1);
-  //     farmBuildProgress++;
-  //     updateFarmBuildProgressText();
-  //     if(farmBuildProgress >= farmBuildProgressMax) {
-  //       document.getElementById("dialogDisplay").innerHTML = "Somehow, your crude pile of live snails has become sturdy enough to house other live snails.";
-  //       document.getElementById('buildFarmButton').style.display='none';
-  //       document.getElementById('farmBuildProgressDisplay').style.display='none';
-  //       document.getElementById('farmBuildLineBreak').style.display='none';
+  buildFarm(): void {
+    if(this.snails >= 1) {
+      this.loseSnails(1);
+      this.farmBuildProgress++;
+      // updateFarmBuildProgressText();
+      if(this.farmBuildProgress >= this.farmBuildProgressMax) {
+        // document.getElementById("dialogDisplay").innerHTML = "Somehow, your crude pile of live snails has become sturdy enough to house other live snails.";
+        // document.getElementById('buildFarmButton').style.display='none';
+        // document.getElementById('farmBuildProgressDisplay').style.display='none';
+        // document.getElementById('farmBuildLineBreak').style.display='none';
         
-  //       document.getElementById('plantSnailButton').style.display='inline';
-  //       document.getElementById('farmSnailsDisplay').style.display='inline';
-  //       document.getElementById('farmSnailsLineBreak').style.display='inline';
-  //       farmFullyBuiltFlag = true;
-  //     }
-  //   }
-  // }
+        // document.getElementById('plantSnailButton').style.display='inline';
+        // document.getElementById('farmSnailsDisplay').style.display='inline';
+        // document.getElementById('farmSnailsLineBreak').style.display='inline';
+        this.farmFullyBuiltFlag = true;
+      }
+    }
+  }
   
-  // function plantSnail() {
-  // if(snails >= 1 && farmSnails < farmSnailsMax) {
-  //   loseSnails(1);
-  //   farmSnails++;
-  //   updateFarmSnailText();
-  //   if(!showFarmLimitFlag && farmSnails >= farmSnailsMax) {
-  //     document.getElementById("dialogDisplay").innerHTML = "The farm is filled to the brim! Seems that you can't stuff any more snails in your snail farm.";
-  //     showFarmLimitFlag = true;
-  //     updateFarmSnailText();
-  //   }
-  // }
-  // }
+  plantSnail(): void {
+  if(this.snails >= 1 && this.farmSnails < this.farmSnailsMax) {
+    this.loseSnails(1);
+    this.farmSnails++;
+    // updateFarmSnailText();
+    if(!this.showFarmLimitFlag && this.farmSnails >= this.farmSnailsMax) {
+      // document.getElementById("dialogDisplay").innerHTML = "The farm is filled to the brim! Seems that you can't stuff any more snails in your snail farm.";
+      this.showFarmLimitFlag = true;
+      // updateFarmSnailText();
+    }
+  }
+  }
   
-  // function gainSnails(snailsToGain) {
-  //   snails += snailsToGain;
-  //   totalAquiredSnails += snailsToGain;
-  //   updateCurrentSnailsText();
+  gainSnails(snailsToGain: number): void {
+    this.snails += snailsToGain;
+    this.totalAquiredSnails += snailsToGain;
+    // updateCurrentSnailsText();
     
-  //   //Consider flags, given the total aquired snails
-  //   if (suggestBuildingFarmFlag == false && totalAquiredSnails >= 150) {
-  //     document.getElementById("dialogDisplay").innerHTML = "Perhaps you could build a snail farm to collect snails more efficiently.";
-  //     document.getElementById('buildFarmButton').style.display='inline';
-  //     document.getElementById('farmBuildProgressDisplay').style.display='inline';
-  //     document.getElementById('farmBuildLineBreak').style.display='inline';
-  //     suggestBuildingFarmFlag = true;
-  //   }
-  // }
+    //Consider flags, given the total aquired snails
+    if (this.suggestBuildingFarmFlag == false && this.totalAquiredSnails >= 150) {
+      // document.getElementById("dialogDisplay").innerHTML = "Perhaps you could build a snail farm to collect snails more efficiently.";
+      // document.getElementById('buildFarmButton').style.display='inline';
+      // document.getElementById('farmBuildProgressDisplay').style.display='inline';
+      // document.getElementById('farmBuildLineBreak').style.display='inline';
+      this.suggestBuildingFarmFlag = true;
+    }
+  }
   
-  // function loseSnails(snailsToLose) {
-  //   snails -= snailsToLose;
-  //   updateCurrentSnailsText();
-  // }
+  loseSnails(snailsToLose: number): void {
+    this.snails -= snailsToLose;
+    // updateCurrentSnailsText();
+  }
   
   
   
   // REMOVE
-  // updateWildSnailsText(): void {
-  //   document.getElementById("wildSnailsDisplay").innerHTML = "Snails in the wild: " 
-  //     + Math.floor(wildSnails) + "/" + wildSnailsMax;
-  // }
+  updateWildSnailsText(): void {
+    this.wildSnailsDisplay = Math.floor(this.wildSnails);
+  }
   
+      // REMOVE
   // function updateCurrentSnailsText() {
   //   document.getElementById("snailsDisplay").innerHTML = "Snails: " + snails;
   // }
   
+    // REMOVE
   // function updateFarmBuildProgressText() {
   //   document.getElementById("farmBuildProgressDisplay").innerHTML = "Snails needed to build farm: " + farmBuildProgress + "/" + farmBuildProgressMax;
   // }
   
+      // REMOVE
   // function updateFarmSnailText() {
   //   if(showFarmLimitFlag) {
   //     document.getElementById("farmSnailsDisplay").innerHTML = "Snails in the farm: " + farmSnails + "/" + farmSnailsMax;
@@ -163,15 +169,16 @@ export class SnailMailComponent implements OnInit {
   //   } 
   // }
   
+  // REMOVE
   // function updateShellsText() {
   //   document.getElementById("shellsDisplay").innerHTML = "Shells: " + shells;
   // }
   
   
-  // function activateTestButton() {
-  //   if(snails < 149) {
-  //     gainSnails(49);
-  //   }
-  // }
+  activateTestButton(): void {
+    if(this.snails < 149) {
+      this.gainSnails(49);
+    }
+  }
 
 }
